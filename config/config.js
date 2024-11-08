e63a1708fb061fde72171513989439ad5317ac90
unknown <yudis.m.aqsha@gmail.com>
2024-11-08 17:25:41 +0800

    change config

--- config/config.js
+++ config/config.js
@@ -10,7 +10,10 @@ module.exports = {
     dialect: "postgres",
     dialectModule: pg,
     dialectOptions: {
-     ssl:false
+     ssl:{
+      require: true,
+      rejectUnauthorized: false,
+    }
     },
   },
   production: {
@@ -21,7 +24,11 @@ module.exports = {
     dialect: "postgres",
     dialectModule: pg,
     dialectOptions: {
-      ssl: false
+      ssl:{
+        require: true,
+        rejectUnauthorized: false,
+      }
+      
     },
   },
 };
